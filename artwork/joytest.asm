8556dfe0d7695a6fd106b8e2848c43a624e0a762
pkali <pawel.kalinowski@gmail.com>
2022-05-11 10:51:22 -0400

    joytest for real hardware

---
+++ artwork/joytest.asm
@@ -0,0 +1,20 @@
+
+    icl '../lib/atari.hea'
+    
+    org $2000
+joytest    
+    mva #0 dmactls
+    
+@
+    lda trig0
+    beq pressed
+    mva #0 colbak
+    beq @-
+pressed
+    lda #$0f
+    ;ora jstick0
+    sta colbak
+    jmp @-
+    
+    run joytest    
+    
