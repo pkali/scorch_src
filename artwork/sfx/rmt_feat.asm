cf08093ceede51b67b979f54c1686d2c3139bced
pkali <pawel.kalinowski@gmail.com>
2022-08-06 16:31:51 -0400

    FEAT update

--- artwork/sfx/rmt_feat.asm
+++ artwork/sfx/rmt_feat.asm
@@ -1,11 +1,11 @@
 ;* --------BEGIN--------
-;* C:\Atari\rmt\rmt128\aa.rmt
+;* C:\Atari\rmt\rmt128\aaa.rmt
 FEAT_SFX        equ 1
 FEAT_GLOBALVOLUMEFADE   equ 0       ;RMTGLOBALVOLUMEFADE variable
 FEAT_NOSTARTINGSONGLINE equ 0
 FEAT_INSTRSPEED     equ 1
-FEAT_CONSTANTSPEED      equ 0       ;(8 times)
-FEAT_COMMAND1       equ 1       ;(11 times)
+FEAT_CONSTANTSPEED      equ 0       ;(18 times)
+FEAT_COMMAND1       equ 1       ;(10 times)
 FEAT_COMMAND2       equ 0       ;(0 times)
 FEAT_COMMAND3       equ 0       ;(0 times)
 FEAT_COMMAND4       equ 0       ;(0 times)
@@ -14,7 +14,7 @@ FEAT_COMMAND6       equ 1       ;(2 times)
 FEAT_COMMAND7SETNOTE        equ 0       ;(0 times)
 FEAT_COMMAND7VOLUMEONLY     equ 0       ;(0 times)
 FEAT_PORTAMENTO     equ 0       ;(0 times)
-FEAT_FILTER     equ 1       ;(44 times)
+FEAT_FILTER     equ 1       ;(22 times)
 FEAT_FILTERG0L      equ 1       ;(13 times)
 FEAT_FILTERG1L      equ 1       ;(22 times)
 FEAT_FILTERG0R      equ 0       ;(0 times)
@@ -32,9 +32,9 @@ FEAT_VOLUMEONLYG2R      equ 0       ;(0 times)
 FEAT_VOLUMEONLYG3R      equ 0       ;(0 times)
 FEAT_TABLETYPE      equ 0       ;(0 times)
 FEAT_TABLEMODE      equ 0       ;(0 times)
-FEAT_TABLEGO        equ 1       ;(1 times)
-FEAT_AUDCTLMANUALSET        equ 1       ;(6 times)
+FEAT_TABLEGO        equ 0       ;(0 times)
+FEAT_AUDCTLMANUALSET        equ 0       ;(0 times)
 FEAT_VOLUMEMIN      equ 0       ;(0 times)
 FEAT_EFFECTVIBRATO      equ 1       ;(1 times)
-FEAT_EFFECTFSHIFT       equ 1       ;(11 times)
+FEAT_EFFECTFSHIFT       equ 0       ;(0 times)
 ;* --------END--------
