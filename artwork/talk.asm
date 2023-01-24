0832d841f4ee375a68cff5036e8314450706590a
Pecusx <Pecus@poczta.fm>
2023-01-24 09:00:43 +0100

    Small talk - small fix.

--- artwork/talk.asm
+++ artwork/talk.asm
@@ -124,8 +124,8 @@ OffensiveTextTableH
     dta >L0,>L1,>L2,>L3,>L4,>L5,>L6,>L7,>L8,>L9,>L10,>L11,>L12,>L13,>L14,>L15,>L16,>L17,>L18,>L19,>L20,>L21,>L22,>L23,>L24,>L25,>L26,>L27,>L28,>L29,>L30,>L31,>L32,>L33,>L34,>L35,>L36,>L37,>L38,>L39,>L40,>L41,>L42,>L43,>L44,>L45,>L46,>L47,>L48,>L49,>L50,>L51,>L52
     dta >L53,>L54,>L55,>L56,>L57,>L58,>L59,>L60,>L61,>L62,>L63,>L64,>L65,>L66,>L67,>L68,>L69,>L70,>L71,>L72,>L73,>L74,>L75,>L76,>L77,>L78,>L79,>L80,>L81,>L82,>L83,>L84,>L85,>L86,>L87,>L88,>L89,>L90,>L91,>L92,>L93,>L94,>L95,>L96,>L97,>L98,>L99,>L100,>L101,>L102,>L103,>L104,>L105,>L106,>L107,>L108,>L109,>L110,>L111,>L112,>L113
     dta >LEND
-NumberOfOffensiveTexts=54
-NumberOfDeffensiveTexts=60
+NumberOfOffensiveTexts=53
+NumberOfDeffensiveTexts=61
 .endp
 hoverFull dta d"MY HOVERCRAFT IS FULL OF EELS!"
 hoverFullEnd
